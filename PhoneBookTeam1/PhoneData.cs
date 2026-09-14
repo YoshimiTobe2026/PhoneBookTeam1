2316679e9dbc193def9f28af43f622039ad13e21
YoshimiTobe2026 <uni202607_13@telework.to>
2026-09-14 10:56:17 +0900

    PhoneDataクラスとdata.txt追加

---
+++ PhoneBookTeam1/PhoneData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookTeam1
+{
+    internal class PhoneData
+    {
+        public string Name { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+    }
+}
