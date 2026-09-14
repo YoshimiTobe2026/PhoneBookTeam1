cb183a1be1c6df63591c1610e4f3cb35cb7f72cf
YoshimiTobe2026 <uni202607_13@telework.to>
2026-09-14 11:33:54 +0900

    ファイル読み込みと検索処理追加

--- PhoneBookTeam1/Form1.cs
+++ PhoneBookTeam1/Form1.cs
@@ -7,19 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PhoneBookTeam1
 {
     public partial class Form1 : Form
     {
+        List<PhoneData> phoneBook;
         public Form1()
         {
             InitializeComponent();
+            phoneBook = new List<PhoneData>();
+            ReadFromFile();
+            foreach (PhoneData data in phoneBook)
+            {
+                nameList.Items.Add(data.Name);
+            }
+        }
+
+        private void ReadFromFile()
+        {
+            string filePath = Path.Combine(Application.StartupPath, "data.txt");
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                while (!file.EndOfStream)
+                {
+                    string line = file.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    string[] data = line.Split(',');
+                    PhoneData phone = new PhoneData();
+                    phone.Name = data[0];
+                    phone.PhoneNumber = data[1];
+                    phoneBook.Add(phone);
+                }
+            }
         }
 
         private void nameList_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            string name = nameList.Text;
+            foreach (PhoneData data in phoneBook)
+            {
+                if (data.Name == name)
+                {
+                    phoneNumber.Text = data.PhoneNumber;
+                    break;
+                }
+            }
         }
     }
 }
