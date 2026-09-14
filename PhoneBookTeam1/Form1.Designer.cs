c400f6a96ad7c7e505f0fb6dfbf49dfecb005ece
YutaNakamura2026 <uni202607_06@telework.to>
2026-09-14 11:03:40 +0900

    ListBoxとphoneNumberを配置

--- PhoneBookTeam1/Form1.Designer.cs
+++ PhoneBookTeam1/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nameList = new System.Windows.Forms.ListBox();
+            this.phoneNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nameList
+            // 
+            this.nameList.FormattingEnabled = true;
+            this.nameList.ItemHeight = 15;
+            this.nameList.Location = new System.Drawing.Point(77, 72);
+            this.nameList.Name = "nameList";
+            this.nameList.Size = new System.Drawing.Size(250, 274);
+            this.nameList.TabIndex = 0;
+            // 
+            // phoneNumber
+            // 
+            this.phoneNumber.Location = new System.Drawing.Point(448, 138);
+            this.phoneNumber.Name = "phoneNumber";
+            this.phoneNumber.Size = new System.Drawing.Size(189, 22);
+            this.phoneNumber.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.phoneNumber);
+            this.Controls.Add(this.nameList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox nameList;
+        private System.Windows.Forms.TextBox phoneNumber;
     }
 }
 
